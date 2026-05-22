240577b5077c2d4605ce45f2004825973437629e
jhicela stefania caicedo bautista <133011097+jhisis@users.noreply.github.com>
2026-05-22 16:22:15 -0500

    Convert user input to uppercase for login check

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function check() {
-    const user = document.getElementById('user').value;
+    const user = document.getElementById('user').value.toUpperCase();
     const pass = document.getElementById('pass').value;
 
     if (user === "NOVIO" && pass === "VIERNES01") {
         document.getElementById('login-box').style.display = 'none';
         document.getElementById('dashboard').style.display = 'block';
-        // OCULTAMOS TAMBIÉN LOS PERSONAJES SI QUIERES
         document.getElementById('rayo').style.display = 'none';
         document.getElementById('sally').style.display = 'none';
     } else {
