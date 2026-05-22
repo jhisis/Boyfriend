7071fc828efd154920f13de8d8ec8fc4942d406f
jhicela stefania caicedo bautista <133011097+jhisis@users.noreply.github.com>
2026-05-22 17:36:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ function check() {
     if (user === "NOVIO" && pass === "VIERNES01") {
         document.getElementById('login-box').style.display = 'none';
         document.getElementById('dashboard').style.display = 'block';
-        document.getElementById('rayo').style.display = 'none';
-        document.getElementById('sally').style.display = 'none';
     } else {
         alert("¡Esa no es la ruta correcta!");
     }
