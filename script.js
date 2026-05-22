674650e4dcc8dfe7a2e0f9e1c0143864a964c74d
jhicela stefania caicedo bautista <133011097+jhisis@users.noreply.github.com>
2026-05-22 15:58:19 -0500

    Update login redirection and alert messages

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function check() {
     if(document.getElementById('user').value === "NOVIO" && document.getElementById('pass').value === "VIERNES01") {
-        window.location.href = "dashboard.html"; // Redirige a la página de las 5 estaciones
+        document.getElementById('login-box').style.display = 'none';
+        document.getElementById('dashboard').style.display = 'block';
     } else {
-        alert("¡Esa no es la ruta correcta!");
+        alert("¡Intenta de nuevo!");
     }
 }
